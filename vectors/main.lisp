e14bec7afecc2539454d704067f187b4b94beb3c
Jan Tatham <jan@sebity.com>
2015-04-06 16:34:18 +0100

    Added: defparams

--- vectors/main.lisp
+++ vectors/main.lisp
@@ -2,6 +2,10 @@
 
 (defparameter *width* 600)
 (defparameter *height* 400)
+(defparameter *x* 0)
+(defparameter *y* 0)
+(defparameter *xspeed* 0)
+(defparameter *yspeed* 0)
 
 (defun help ()
   (format t "Vectors~%")
