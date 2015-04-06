32613522f48ac9ca53f98e952dff29a67d78286d
Jan Tatham <jan@sebity.com>
2015-04-06 14:54:37 +0100

    Updated: maths and primitives files

--- maths.lisp
+++ maths.lisp
@@ -1,4 +1,14 @@
-(in-package #:nature-of-code)
+(in-package #:maths)
 
 (defun square (x)
   (* x x))
+
+(defun random-gaussian (mean std-dev)
+  (+ mean (* std-dev 
+	     (sqrt (* -2 (log (random 1.0))))
+	     (cos (* 2 pi (random 1.0))))))
+
+(defun map-range (value min1 max1 min2 max2)
+  (+ min2 
+     (* (- max2 min2) 
+	(/ (- value min1) (- max1 min1)))))
--- primitives.lisp
+++ primitives.lisp
@@ -15,6 +15,14 @@
 		:color (sdl:color :r r :g g :b b)))
 
 
+;;;; DRAW-BOX-STROKE function
+
+(defun draw-box-stroke (x y w h rgb1 rgb2)
+  (sdl:draw-box (sdl:rectangle :x x :y y :w w :h h)
+		:color (sdl:color :r (first rgb1) :g (second rgb1) :b (third rgb1))
+		:stroke-color (sdl:color :r (first rgb2) :g (second rgb2) :b (third rgb2))))
+
+
 ;;;; DRAW-LINE function
 
 (defun draw-line (x0 y0 x1 y1 r g b)
@@ -33,6 +41,13 @@
 (defun draw-circle-filled (x y rad r g b)
   (sdl:draw-filled-circle-* x y rad
 		     :color (sdl:color :r r :g g :b b)))
+
+
+;;;; DRAW-CIRCLE-FILLED-ALPHA function
+
+(defun draw-circle-filled-alpha (x y rad r g b alpha)
+  (sdl:draw-filled-circle-* x y rad
+		     :color (sdl:color :r r :g g :b b :a alpha)))
 
 
 ;;;; DRAW-ELLIPSE-FILLED function
