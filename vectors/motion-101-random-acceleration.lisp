039d56ffb9fad2f81fe06954410e4a97ac1ec909
Jan Tatham <jan@sebity.com>
2015-04-15 16:42:06 +0100

    Added: motion 101 random acceleration example

---
+++ vectors/motion-101-random-acceleration.lisp
@@ -0,0 +1,62 @@
+(in-package #:noc-vectors)
+
+;;;; update-ex-1-9
+(defun update-ex-1-9 (loc vel acc)
+  (random-2d-vectors acc)
+  (mult-vectors acc (random 2))
+
+  (add-vectors vel acc)
+  (limit-vectors vel *topspeed*)
+  (add-vectors loc vel))
+
+
+;;;; check-edges-ex-1-9
+(defun check-edges-ex-1-9 (loc)
+  (cond ((> (x loc) *width*) (setf (x loc) 0))
+	((< (x loc) 0) (setf (x loc) *width*)))
+
+  (cond ((> (y loc) *height*) (setf (y loc) 0))
+	((< (y loc) 0) (setf (y loc) *height*))))
+
+
+;;;; display-ex-1-9
+(defun display-ex-1-9 (loc)
+  (draw-ellipse-filled (floor (x loc)) (floor (y loc)) 24 24 0 0 0))
+
+
+;;;; render-ex-1-9
+(defun render-ex-1-9 ()
+  (update-swank)
+  (sdl:clear-display sdl:*white*)
+  (update-ex-1-9 *location* *velocity* *acceleration*)
+  (check-edges-ex-1-9 *location*)
+  (display-ex-1-9 *location*)
+  (sdl:update-display))
+
+
+;;;; setup-ex-1-9
+(defun setup-ex-1-9 ()
+  (setf *width* 600)
+  (setf *height* 360)
+  (setf *location* (make-instance 'pvector :x (/ *width* 2) :y (/ *height* 2)))
+  (setf *velocity* (make-instance 'pvector :x 0 :y 0))
+  (setf *acceleration* (make-instance 'pvector :x 0 :y 0))
+  (setf *topspeed* 6))
+
+
+(defun motion-101-random-acceleration ()
+  (setup-ex-1-9)
+  (sdl:with-init (sdl:sdl-init-video)
+    (sdl:window *width* *height* :title-caption "Motion 101 (Random Acceleration)")
+    (setf (sdl:frame-rate) 30)
+    (sdl:clear-display sdl:*white*)
+
+    (sdl:with-events ()
+      (:quit-event () t)
+      (:key-down-event (:key key)
+		       (case key
+			 (:sdl-key-escape (sdl:push-quit-event))))
+      (:key-up-event (:key key)
+		     (case key))
+      (:idle ()
+	     (render-ex-1-9)))))
