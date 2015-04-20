584b8c20467207a0171a3eef77945cc002aa63d5
Jan Tatham <jan@sebity.com>
2015-04-20 21:57:11 +0100

    Added: apply-force function Updated: help function

--- forces/main.lisp
+++ forces/main.lisp
@@ -15,8 +15,15 @@
    (mass :initarg :mass :initform 1 :accessor mass)))
 
 
+;;;; apply-force
+(defun apply-force (m force)
+  (div force (mass m))
+  (add (acceleration m) force)) 
+
+
 (defun help ()
   (format t "Introduction~%")
   (format t "============~%~%")
   (format t "> forces-2-1~%")
-  (format t "> forces-2-2~%"))
+  (format t "> forces-2-2~%")
+  (format t "> forces-2-3~%"))
